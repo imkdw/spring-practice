bf5d39de9bb8b1bd9f6d569a8f91bf73ebab2beb
imkdw <imkdw@kakao.com>
2024-01-25 13:30:52 +0900

    (~섹션4) 스프링 빈과 의존관계완료

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,7 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
 import java.util.List;
